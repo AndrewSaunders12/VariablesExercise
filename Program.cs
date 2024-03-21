7260f0258bafdb467823d20822f8f5cd90a251c4
AndrewSaunders12 <ninjaraider04@gmail.com>
2024-03-21 13:17:18 -0400

    Story with variables

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace VariablesExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number; //declaring a variable
+            number = 15; //initializing a variable
+
+            string name = "Andrew";
+
+            double payment = 25.79;
+
+            Console.WriteLine($"{name} had to wait {number} minutes for his buffalo wings that were {payment} dollars.");
+        }
+    }
+}
